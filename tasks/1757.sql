b4285ea4723cee4af06caaeed99af90bec45c351
Darya Shynkevich <shynkevich.darya@gmail.com>
2022-11-12 15:31:00 +0100

    Add 1757

---
+++ tasks/1757.sql
@@ -0,0 +1,19 @@
+# Write an SQL query to find the ids of products that are both low fat and recyclable.
+#
+# Return the result table in any order.
+#
+# The query result format is in the following example.
+
+
+CREATE TABLE Products (product_id INT, low_fats ENUM('Y', 'N'), recyclable ENUM('Y', 'N'));
+
+
+INSERT INTO Products(product_id, low_fats, recyclable) VALUES
+        (0, 'Y', 'N'),
+        (1, 'Y', 'Y'),
+        (2, 'N', 'Y'),
+        (3, 'Y', 'Y'),
+        (4, 'N', 'N');
+
+
+SELECT product_id from Products WHERE low_fats = 'Y' AND recyclable = 'Y';
