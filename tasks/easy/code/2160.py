52285c8e52e567baae9de6c0231f8555486cf23d
Darya Shynkevich <shynkevich.darya@gmail.com>
2022-11-24 00:41:12 +0100

    Add 2160

--- tasks/easy/code/2160.py
+++ tasks/easy/code/2160.py
@@ -6,39 +6,32 @@
 # Some of the possible pairs [new1, new2] are [22, 93], [23, 92], [223, 9] and [2, 329].
 # Return the minimum possible sum of new1 and new2.
 
+from itertools import permutations, combinations
+
+
 class Solution:
 
     def minimumSum(self, num: int) -> int:
-        input_number_str = str(num)
 
-        n = len(input_number_str)
+        min_sum = num
+        for combination in list(permutations(str(num))):
+            number_1 = int(f'{combination[0]}{combination[1]}')
+            number_2 = int(f'{combination[2]}{combination[3]}')
+            min_sum = min(number_1 + number_2, min_sum)
 
-        part_variants = set()
-        for i in range(n):
-            for j in range(i + 1, n):
-                part_variants.add((
-                    int(f'{input_number_str[i]}{input_number_str[j]}'),
-                    int(f'{input_number_str[n - i - 1]}{input_number_str[n - j]}'),
-                ))
-                part_variants.add((
-                    int(f'{input_number_str[i]}{input_number_str[j]}'),
-                    int(f'{input_number_str[n - i - 1]}{input_number_str[n - j - 1]}'),
-                ))
+            number_1 = int(f'{combination[0]}{combination[1]}{combination[2]}')
+            number_2 = int(f'{combination[3]}')
+            min_sum = min(number_1 + number_2, min_sum)
 
-            for k in range(i + 2, n):
-                part_variants.add(int(f'{input_number_str[i]}{input_number_str[k-1]}{input_number_str[k]}'))
-
-        print(part_variants)
-        return
-
+        return min_sum
 
 
 if __name__ == "__main__":
     solution = Solution()
 
-    print(solution.minimumSum(num=2932))
+    assert solution.minimumSum(num=2932) == 52
 
-    # assert solution.minimumSum(num=2932) == 52
-    #
-    # assert solution.minimumSum(num=4009) == 13
+    assert solution.minimumSum(num=4009) == 13
+
+    assert solution.minimumSum(num=1000) == 1
 
