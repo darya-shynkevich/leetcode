c2be49db6325fb17eef5878ab05a6680281da6d7
Darya Shynkevich <shynkevich.darya@gmail.com>
2022-12-15 22:46:24 +0100

    Add testcase

--- tasks/easy/code/1221.py
+++ tasks/easy/code/1221.py
@@ -35,3 +35,5 @@ if __name__ == "__main__":
     assert solution.balancedStringSplit(s="RLRRLLRLRL") == 4
 
     assert solution.balancedStringSplit(s="RLRRRLLRLL") == 2
+
+    assert solution.balancedStringSplit(s="RL") == 1
