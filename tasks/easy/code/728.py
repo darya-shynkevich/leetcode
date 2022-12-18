64b58d3dc485f2841edf32065f259cb7ef7ae384
Darya Shynkevich <shynkevich.darya@gmail.com>
2022-12-18 19:06:52 +0100

    Add setup for 728

---
+++ tasks/easy/code/728.py
@@ -0,0 +1,17 @@
+# A self-dividing number is a number that is divisible by every digit it contains.
+#
+# For example, 128 is a self-dividing number because 128 % 1 == 0, 128 % 2 == 0, and 128 % 8 == 0.
+# A self-dividing number is not allowed to contain the digit zero.
+#
+# Given two integers left and right, return a list of all the self-dividing numbers in the range [left, right].
+
+# Constraints:
+#
+# 1 <= left <= right <= 104
+from typing import List
+
+
+class Solution:
+    def selfDividingNumbers(self, left: int, right: int) -> List[int]:
+        pass
+
