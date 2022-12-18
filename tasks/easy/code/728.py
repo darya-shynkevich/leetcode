768b04ca4d53119246fe0ede32582ef238ba11af
Darya Shynkevich <shynkevich.darya@gmail.com>
2022-12-18 19:15:00 +0100

    Add 728

--- tasks/easy/code/728.py
+++ tasks/easy/code/728.py
@@ -12,6 +12,34 @@ from typing import List
 
 
 class Solution:
-    def selfDividingNumbers(self, left: int, right: int) -> List[int]:
-        pass
 
+    def is_self_dividing(self, number: int):
+        input_number = number
+        while number:
+            target_digit = number % 10
+            if target_digit == 0:
+                return False
+
+            if input_number % target_digit:
+                return False
+
+            number = number // 10
+
+        return True
+
+    def selfDividingNumbers(self, left: int, right: int) -> List[int]:
+        result = []
+
+        for number in range(left, right + 1):
+            if self.is_self_dividing(number):
+                result.append(number)
+
+        return result
+
+
+if __name__ == "__main__":
+    solution = Solution()
+
+    assert solution.selfDividingNumbers(left=1, right=22) == [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 15, 22]
+
+    assert solution.selfDividingNumbers(left=47, right=85) == [48, 55, 66, 77]
