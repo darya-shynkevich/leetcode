f38546a77e2e3a006edc7a158c1f2342efd62846
Darya Shynkevich <shynkevich.darya@gmail.com>
2022-12-19 19:43:00 +0100

    Add 577

--- tasks/easy/sql/577.sql
+++ tasks/easy/sql/577.sql
@@ -9,6 +9,7 @@ DROP table If Exists Bonus;
 
 Create table If Not Exists Employee (empId int, name varchar(255), supervisor int, salary int);
 Create table If Not Exists Bonus (empId int, bonus int);
+
 Truncate table Employee;
 insert into Employee (empId, name, supervisor, salary) values ('3', 'Brad', null, '4000');
 insert into Employee (empId, name, supervisor, salary) values ('1', 'John', '3', '1000');
@@ -17,3 +18,7 @@ insert into Employee (empId, name, supervisor, salary) values ('4', 'Thomas', '3
 Truncate table Bonus;
 insert into Bonus (empId, bonus) values ('2', '500');
 insert into Bonus (empId, bonus) values ('4', '2000');
+
+SELECT name, bonus
+FROM Employee as E LEFT JOIN Bonus B on E.empId = B.empId
+WHERE bonus < 1000 or ISNULL(bonus);
