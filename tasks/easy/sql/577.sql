e73d315cf0413df42c0e05e5a596fdd5c9220a07
Darya Shynkevich <shynkevich.darya@gmail.com>
2022-12-19 19:38:53 +0100

    Add setup for 577

---
+++ tasks/easy/sql/577.sql
@@ -0,0 +1,19 @@
+# Write an SQL query to report the name and bonus amount of each employee with a bonus less than 1000.
+#
+# Return the result table in any order.
+#
+# The query result format is in the following example.
+
+DROP table If Exists Employee;
+DROP table If Exists Bonus;
+
+Create table If Not Exists Employee (empId int, name varchar(255), supervisor int, salary int);
+Create table If Not Exists Bonus (empId int, bonus int);
+Truncate table Employee;
+insert into Employee (empId, name, supervisor, salary) values ('3', 'Brad', null, '4000');
+insert into Employee (empId, name, supervisor, salary) values ('1', 'John', '3', '1000');
+insert into Employee (empId, name, supervisor, salary) values ('2', 'Dan', '3', '2000');
+insert into Employee (empId, name, supervisor, salary) values ('4', 'Thomas', '3', '4000');
+Truncate table Bonus;
+insert into Bonus (empId, bonus) values ('2', '500');
+insert into Bonus (empId, bonus) values ('4', '2000');
