3738fbb681a886eda57cfc6e213d8b431e2a2ea1
Darya Shynkevich <shynkevich.darya@gmail.com>
2022-12-19 19:49:47 +0100

    Add 1484

--- tasks/easy/sql/1484.sql
+++ tasks/easy/sql/1484.sql
@@ -19,3 +19,6 @@ insert into Activities (sell_date, product) values ('2020-06-01', 'Bible');
 insert into Activities (sell_date, product) values ('2020-06-02', 'Mask');
 insert into Activities (sell_date, product) values ('2020-05-30', 'T-Shirt');
 
+
+SELECT sell_date, COUNT(distinct product) AS num_sold, GROUP_CONCAT(distinct product) AS products
+FROM Activities GROUP BY sell_date;
