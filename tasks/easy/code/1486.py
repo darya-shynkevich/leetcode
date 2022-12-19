e489e9b7976531a25d6907bd7b38898feee0f1e7
Darya Shynkevich <shynkevich.darya@gmail.com>
2022-12-19 19:27:12 +0100

    Add setup for 1486

---
+++ tasks/easy/code/1486.py
@@ -0,0 +1,24 @@
+# You are given an integer n and an integer start.
+#
+# Define an array nums where nums[i] = start + 2 * i (0-indexed) and n == nums.length.
+#
+# Return the bitwise XOR of all elements of nums.
+
+# Constraints:
+#
+# 1 <= n <= 1000
+# 0 <= start <= 1000
+# n == nums.length
+
+
+class Solution:
+    def xorOperation(self, n: int, start: int) -> int:
+        pass
+
+
+if __name__ == "__main__":
+    solution = Solution()
+
+    assert solution.xorOperation(n=5, start=0) == 8
+
+    assert solution.xorOperation(n=4, start=3) == 8
