f25e7dd61b1a62a595e394573ff11547ec7799bb
Darya Shynkevich <shynkevich.darya@gmail.com>
2022-12-19 19:34:48 +0100

    Add 1486

--- tasks/easy/code/1486.py
+++ tasks/easy/code/1486.py
@@ -13,7 +13,11 @@
 
 class Solution:
     def xorOperation(self, n: int, start: int) -> int:
-        pass
+        result = start
+        for i in range(1, n):
+            result = result ^ (start + 2 * i)
+
+        return result
 
 
 if __name__ == "__main__":
