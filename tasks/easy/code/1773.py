a1a83b75143c0ab635487af08e02fa67be07e567
Darya Shynkevich <shynkevich.darya@gmail.com>
2022-12-19 19:20:32 +0100

    Add setup for 1773

---
+++ tasks/easy/code/1773.py
@@ -0,0 +1,37 @@
+# You are given an array items, where each items[i] = [typei, colori, namei] describes the type,
+# color, and name of the ith item. You are also given a rule represented by two strings, ruleKey and ruleValue.
+#
+# The ith item is said to match the rule if one of the following is true:
+#
+# ruleKey == "type" and ruleValue == typei.
+# ruleKey == "color" and ruleValue == colori.
+# ruleKey == "name" and ruleValue == namei.
+# Return the number of items that match the given rule.
+
+# Constraints:
+#
+# 1 <= items.length <= 104
+# 1 <= typei.length, colori.length, namei.length, ruleValue.length <= 10
+# ruleKey is equal to either "type", "color", or "name".
+# All strings consist only of lowercase letters.
+
+from typing import List
+
+
+class Solution:
+    def countMatches(self, items: List[List[str]], ruleKey: str, ruleValue: str) -> int:
+        pass
+
+
+if __name__ == "__main__":
+    solution = Solution()
+
+    assert solution.countMatches(
+        items=[["phone", "blue", "pixel"], ["computer", "silver", "lenovo"], ["phone", "gold", "iphone"]],
+        ruleKey="color", ruleValue="silver"
+    ) == 1
+
+    assert solution.countMatches(
+        items=[["phone", "blue", "pixel"], ["computer", "silver", "phone"], ["phone", "gold", "iphone"]],
+        ruleKey="type", ruleValue="phone"
+    ) == 2
