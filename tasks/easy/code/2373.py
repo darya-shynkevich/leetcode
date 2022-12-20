dd074c31302f0bde696893be8a934931e1bcea96
Darya Shynkevich <shynkevich.darya@gmail.com>
2022-12-20 14:16:05 +0100

    Add solution for 2373

--- tasks/easy/code/2373.py
+++ tasks/easy/code/2373.py
@@ -17,7 +17,14 @@ from typing import List
 
 class Solution:
     def largestLocal(self, grid: List[List[int]]) -> List[List[int]]:
-        pass
+        n = len(grid)
+        result = [[0]*(n-2) for _ in range(n-2)]
+
+        for i in range(n - 2):
+            for j in range(n - 2):
+                result[i][j] = max(grid[ii][jj] for ii in range(i, i+3) for jj in range(j, j+3))
+
+        return result
 
 
 if __name__ == "__main__":
@@ -30,12 +37,16 @@ if __name__ == "__main__":
         [6, 2, 2, 2]
     ]) == [[9, 9], [8, 6]]
 
-    assert solution.largestLocal(
-        grid=[
-            [1, 1, 1, 1, 1],
-            [1, 1, 1, 1, 1],
-            [1, 1, 2, 1, 1],
-            [1, 1, 1, 1, 1],
-            [1, 1, 1, 1, 1]
-        ]
-    ) == [[2, 2, 2], [2, 2, 2], [2, 2, 2]]
+    # assert solution.largestLocal(
+    #     grid=[
+    #         [1, 1, 1, 1, 1],
+    #         [1, 1, 1, 1, 1],
+    #         [1, 1, 2, 1, 1],
+    #         [1, 1, 1, 1, 1],
+    #         [1, 1, 1, 1, 1]
+    #     ]
+    # ) == [
+    #     [2, 2, 2],
+    #     [2, 2, 2],
+    #     [2, 2, 2]
+    # ]
