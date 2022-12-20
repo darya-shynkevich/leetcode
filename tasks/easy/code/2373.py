b00ecefb840ed5c51f0836726197dbc280c75029
Darya Shynkevich <shynkevich.darya@gmail.com>
2022-12-20 13:52:50 +0100

    Add setup for 2373

---
+++ tasks/easy/code/2373.py
@@ -0,0 +1,41 @@
+# You are given an n x n integer matrix grid.
+#
+# Generate an integer matrix maxLocal of size (n - 2) x (n - 2) such that:
+#
+# maxLocal[i][j] is equal to the largest value of the 3 x 3 matrix in grid centered around row i + 1 and column j + 1.
+# In other words, we want to find the largest value in every contiguous 3 x 3 matrix in grid.
+#
+# Return the generated matrix.
+
+# Constraints:
+#
+# n == grid.length == grid[i].length
+# 3 <= n <= 100
+# 1 <= grid[i][j] <= 100
+from typing import List
+
+
+class Solution:
+    def largestLocal(self, grid: List[List[int]]) -> List[List[int]]:
+        pass
+
+
+if __name__ == "__main__":
+    solution = Solution()
+
+    assert solution.largestLocal(grid=[
+        [9, 9, 8, 1],
+        [5, 6, 2, 6],
+        [8, 2, 6, 4],
+        [6, 2, 2, 2]
+    ]) == [[9, 9], [8, 6]]
+
+    assert solution.largestLocal(
+        grid=[
+            [1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1],
+            [1, 1, 2, 1, 1],
+            [1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1]
+        ]
+    ) == [[2, 2, 2], [2, 2, 2], [2, 2, 2]]
