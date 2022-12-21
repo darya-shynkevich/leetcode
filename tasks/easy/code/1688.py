9c4e6bbbf69349ce0f6f3a6f4e1dc0982b035bc9
Darya Shynkevich <shynkevich.darya@gmail.com>
2022-12-21 18:53:39 +0100

    Add setup for 1688

---
+++ tasks/easy/code/1688.py
@@ -0,0 +1,23 @@
+# You are given an integer n, the number of teams in a tournament that has strange rules:
+#
+# If the current number of teams is even, each team gets paired with another team.
+# A total of n / 2 matches are played, and n / 2 teams advance to the next round.
+# If the current number of teams is odd, one team randomly advances in the tournament, and the rest gets paired.
+# A total of (n - 1) / 2 matches are played, and (n - 1) / 2 + 1 teams advance to the next round.
+# Return the number of matches played in the tournament until a winner is decided.
+
+# Constraints:
+#
+# 1 <= n <= 200
+
+class Solution:
+    def numberOfMatches(self, n: int) -> int:
+        pass
+
+
+if __name__ == "__main__":
+    solution = Solution()
+
+    assert solution.numberOfMatches(n=7) == 6
+
+    assert solution.numberOfMatches(n=14) == 13
