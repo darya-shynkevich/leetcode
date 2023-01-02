aaee962fe12086de2b3a1e1f4bc48ede429809b5
Darya Shynkevich <shynkevich.darya@gmail.com>
2023-01-02 23:24:27 +0100

    Add setup for 2006

---
+++ tasks/easy/code/2006.py
@@ -0,0 +1,29 @@
+# Given an integer array nums and an integer k, return the number of pairs (i, j)
+# where i < j such that |nums[i] - nums[j]| == k.
+#
+# The value of |x| is defined as:
+#
+# x if x >= 0.
+# -x if x < 0.
+
+# Constraints:
+#
+# 1 <= nums.length <= 200
+# 1 <= nums[i] <= 100
+# 1 <= k <= 99
+from typing import List
+
+
+class Solution:
+    def countKDifference(self, nums: List[int], k: int) -> int:
+        pass
+
+
+if __name__ == "__main__":
+    solution = Solution()
+
+    assert solution.countKDifference(nums=[1, 2, 2, 1], k=1) == 4
+
+    assert solution.countKDifference(nums=[1, 3], k=3) == 0
+
+    assert solution.countKDifference(nums=[3, 2, 1, 5, 4], k=2) == 3
