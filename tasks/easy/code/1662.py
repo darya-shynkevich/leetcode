3b8e58ebd1a70319da84610b6d48be474352d8c8
Darya Shynkevich <shynkevich.darya@gmail.com>
2023-01-03 00:17:18 +0100

    Add solution for 1662

--- tasks/easy/code/1662.py
+++ tasks/easy/code/1662.py
@@ -13,7 +13,7 @@ from typing import List
 
 class Solution:
     def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
-        pass
+        return ''.join(word1) == ''.join(word2)
 
 
 if __name__ == "__main__":
