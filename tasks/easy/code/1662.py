49da13c52e1b20012e8657e241535f80663d4771
Darya Shynkevich <shynkevich.darya@gmail.com>
2023-01-03 00:17:47 +0100

    Add time and space complexity for 1662

--- tasks/easy/code/1662.py
+++ tasks/easy/code/1662.py
@@ -13,6 +13,9 @@ from typing import List
 
 class Solution:
     def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
+
+        # Time: O(N), Space: O(N)
+
         return ''.join(word1) == ''.join(word2)
 
 
