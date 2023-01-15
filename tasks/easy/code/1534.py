cc35751a1d05b48f2dd247396d25ab4a6d753954
Darya Shynkevich <shynkevich.darya@gmail.com>
2023-01-16 00:15:16 +0100

    Add solution for 1534

--- tasks/easy/code/1534.py
+++ tasks/easy/code/1534.py
@@ -20,7 +20,24 @@ from typing import List
 
 class Solution:
     def countGoodTriplets(self, arr: List[int], a: int, b: int, c: int) -> int:
-        pass
+        good_triplets_count = 0
+
+        n = len(arr)
+
+        for i in range(n - 2):
+            for j in range(i + 1, n - 1):
+                if abs(arr[i] - arr[j]) > a:
+                    continue
+
+                for k in range(j + 1, n):
+                    if all([
+                        abs(arr[i] - arr[j]) <= a,
+                        abs(arr[j] - arr[k]) <= b,
+                        abs(arr[i] - arr[k]) <= c
+                    ]):
+                        good_triplets_count += 1
+
+        return good_triplets_count
 
 
 if __name__ == "__main__":
