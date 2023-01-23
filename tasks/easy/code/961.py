0be35e8a62ffa597c392e278a37000b24d897774
Darya Shynkevich <shynkevich.darya@gmail.com>
2023-01-23 23:29:24 +0100

    Add solution for 961

---
+++ tasks/easy/code/961.py
@@ -0,0 +1,34 @@
+# You are given an integer array nums with the following properties:
+#
+# nums.length == 2 * n.
+# nums contains n + 1 unique elements.
+# Exactly one element of nums is repeated n times.
+# Return the element that is repeated n times.
+
+# Constraints:
+#
+# 2 <= n <= 5000
+# nums.length == 2 * n
+# 0 <= nums[i] <= 104
+# nums contains n + 1 unique elements and one of them is repeated exactly n times.
+from typing import List
+
+
+class Solution:
+    def repeatedNTimes(self, nums: List[int]) -> int:
+        nums_set = set()
+        for num in nums:
+            if num in nums_set:
+                return num
+
+            nums_set.add(num)
+
+
+if __name__ == "__main__":
+    solution = Solution()
+
+    assert solution.repeatedNTimes(nums=[1, 2, 3, 3]) == 3
+
+    assert solution.repeatedNTimes(nums=[2, 1, 2, 5, 3, 2]) == 2
+
+    assert solution.repeatedNTimes(nums=[5, 1, 5, 2, 5, 3, 5, 4]) == 5
