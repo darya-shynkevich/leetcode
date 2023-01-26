55a1175e09166e68a4bb14b46fa828d20282da8c
Darya Shynkevich <shynkevich.darya@gmail.com>
2023-01-26 23:38:56 +0100

    Add solution for 1564

---
+++ tasks/easy/code/1464.py
@@ -0,0 +1,24 @@
+# Given the array of integers nums, you will choose two different indices i and j of that array.
+# Return the maximum value of (nums[i]-1)*(nums[j]-1).
+
+# Constraints:
+#
+# 2 <= nums.length <= 500
+# 1 <= nums[i] <= 10^3
+from typing import List
+
+
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        nums.sort()
+        return (nums[-1] - 1) * (nums[-2] - 1)
+
+
+if __name__ == "__main__":
+    solution = Solution()
+
+    assert solution.maxProduct(nums=[3, 4, 5, 2]) == 12
+
+    assert solution.maxProduct(nums=[1, 5, 4, 5]) == 16
+
+    assert solution.maxProduct(nums=[3, 7]) == 12
