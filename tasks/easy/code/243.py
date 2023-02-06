8e1c20f1db6503f2813bad7639d2361f1b5bd07b
Darya Shynkevich <shynkevich.darya@gmail.com>
2023-02-06 14:37:04 +0100

    Add solution for 243

--- tasks/easy/code/243.py
+++ tasks/easy/code/243.py
@@ -3,17 +3,34 @@
 
 # Constraints:
 #
-# 2 <= wordsDict.length <= 3 * 104
+# 2 <= wordsDict.length <= 3 * 10^4
 # 1 <= wordsDict[i].length <= 10
 # wordsDict[i] consists of lowercase English letters.
 # word1 and word2 are in wordsDict.
 # word1 != word2
+import math
 from typing import List
 
 
 class Solution:
     def shortestDistance(self, wordsDict: List[str], word1: str, word2: str) -> int:
-        pass
+
+        # Time: O(n), Space: O91)
+
+        distance = math.inf
+
+        index_1 = index_2 = None
+        for i, word in enumerate(wordsDict):
+
+            if word == word1:
+                index_1 = i
+            elif word == word2:
+                index_2 = i
+
+            if index_1 is not None and index_2 is not None:
+                distance = min(abs(index_2 - index_1), distance)
+
+        return distance
 
 
 if __name__ == "__main__":
@@ -27,3 +44,4 @@ if __name__ == "__main__":
         wordsDict=["practice", "makes", "perfect", "coding", "makes"], word1="makes", word2="coding"
     ) == 1
 
+    assert solution.shortestDistance(wordsDict=["a", "a", "b", "b"], word1="a", word2="b") == 1
