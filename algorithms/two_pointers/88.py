3885860650771ab26f951de3d18de867e39b17e8
Darya Shynkevich <shynkevich.darya@gmail.com>
2023-06-11 16:28:25 +0200

    Fix for 88

--- algorithms/two_pointers/88.py
+++ algorithms/two_pointers/88.py
@@ -27,17 +27,15 @@ class Solution:
         Do not return anything, modify nums1 in-place instead.
         """
 
-        if m == 0:
-            nums1 = nums2
-            return nums1
-
         if m + n == 1:
-            nums1 = nums1 or nums2
+            nums1[0] = nums1[0] or nums2[0]
             return nums1
 
         i = j = 0
 
         while i < n + m and j < n:
+
+            print(nums1, nums2, i, j)
 
             if nums2[j] < nums1[i]:
                 nums1[i], nums2[j] = nums2[j], nums1[i]
@@ -53,10 +51,12 @@ class Solution:
 if __name__ == "__main__":
     solution = Solution()
 
-    assert solution.merge(nums1=[1, 2, 3, 0, 0, 0], m=3, nums2=[2, 5, 6], n=3) == [1, 2, 2, 3, 5, 6]
+    # assert solution.merge(nums1=[1, 2, 3, 0, 0, 0], m=3, nums2=[2, 5, 6], n=3) == [1, 2, 2, 3, 5, 6]
+    #
+    # assert solution.merge(nums1=[1, 2, 2, 3, 5, 0, 0, 0], m=5, nums2=[2, 5, 6], n=3) == [1, 2, 2, 2, 3, 5, 5, 6]
+    #
+    # assert solution.merge(nums1=[1], m=1, nums2=[], n=0) == [1]
+    #
+    # assert solution.merge(nums1=[0], m=0, nums2=[1], n=1) == [1]
 
-    assert solution.merge(nums1=[1, 2, 2, 3, 5, 0, 0, 0], m=5, nums2=[2, 5, 6], n=3) == [1, 2, 2, 2, 3, 5, 5, 6]
-
-    assert solution.merge(nums1=[1], m=1, nums2=[], n=0) == [1]
-
-    assert solution.merge(nums1=[0], m=0, nums2=[1], n=1) == [1]
+    assert solution.merge(nums1=[4, 5, 6, 0, 0, 0], m=3, nums2=[1, 2, 3], n=3) == [1, 2, 2, 3, 5, 6]
