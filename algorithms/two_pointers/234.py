a659949b34e90c80995a209a181ea85f107ea9c1
Darya Shynkevich <shynkevich.darya@gmail.com>
2023-08-09 23:49:39 +0200

    Practice 2 pointers: 234

--- algorithms/two_pointers/234.py
+++ algorithms/two_pointers/234.py
@@ -13,31 +13,10 @@ class Solution:
         if head is None:
             return False
 
-        node = head
-        list_len = 1
-        while node.next:
-            list_len += 1
-            node = node.next
-
-        tail = node
-
-        i = 0
-        medium_node, node = None, head
-        while node.next:
-            next_node = node.next.next
-            if next_node is None:
-                break
-
-            if i >= list_len // 2 - 1:
-                breakpoint()
-                next_node.next = node
-
-            node = next_node
-            i += 1
+        tail = self.reverse_list(head=self.get_medium_node(head))
 
         node = head
-        while node:
-            print(node.val, tail.val)
+        while node and tail:
             if node.val != tail.val:
                 return False
 
@@ -45,6 +24,40 @@ class Solution:
             tail = tail.next
 
         return True
+
+    def get_llist_len(self, head):
+        node = head
+        list_len = 1
+        while node.next:
+            list_len += 1
+            node = node.next
+
+        return list_len
+
+    def get_medium_node(self, head):
+        list_len = self.get_llist_len(head=head)
+
+        i = 0
+        node = head
+        while node:
+            if i == list_len // 2 - 1:
+                return node.next
+
+            node = node.next
+            i += 1
+
+    def reverse_list(self, head):
+        node = head
+
+        prev_node = None
+        while node:
+             next_node = node.next
+             node.next = prev_node
+             prev_node = node
+             node = next_node
+
+        return prev_node
+
 
 
 if __name__ == "__main__":
@@ -54,15 +67,15 @@ if __name__ == "__main__":
     node_3 = ListNode(next=node_4, x=2)
     node_2 = ListNode(next=node_3, x=2)
     node_1 = ListNode(next=node_2, x=1)
-    
+
     assert solution.isPalindrome(head=node_1)
 
-    # solution = Solution()
-    #
-    # node_5 = ListNode(next=None, x=1)
-    # node_4 = ListNode(next=node_5, x=2)
-    # node_3 = ListNode(next=node_4, x=3)
-    # node_2 = ListNode(next=node_3, x=2)
-    # node_1 = ListNode(next=node_2, x=1)
-    #
-    # assert solution.isPalindrome(head=node_1)
+    solution = Solution()
+
+    node_5 = ListNode(next=None, x=1)
+    node_4 = ListNode(next=node_5, x=2)
+    node_3 = ListNode(next=node_4, x=3)
+    node_2 = ListNode(next=node_3, x=2)
+    node_1 = ListNode(next=node_2, x=1)
+
+    assert solution.isPalindrome(head=node_1)
