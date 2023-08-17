158f7b0ffe352f1fe9ae717596f8b35870cd8b5d
Darya Shynkevich <shynkevich.darya@gmail.com>
2023-08-17 23:56:07 +0200

    Practice 2 pointers: 350

--- algorithms/two_pointers/350.py
+++ algorithms/two_pointers/350.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from typing import List
 
 
@@ -6,17 +7,15 @@ class Solution:
         if len(nums1) == len(nums2) and nums1 == nums2:
             return nums1
 
-        if len(nums2) > len(nums1):
-            nums1, nums2 = nums2, nums1
+        hash_map1 = defaultdict(int)
+        for num in nums1:
+            hash_map1[num] += 1
 
         result = []
-        i = j = 0
-        while i < len(nums1) - 1:
-            if nums1[i] == nums2[j]:
-                result.extend([nums1[i]])
-                j += 1
-
-            i += 1
+        for num in nums2:
+            if num in hash_map1 and hash_map1[num] > 0:
+                result.append(num)
+                hash_map1[num] -= 1
 
         return result
 
@@ -26,7 +25,7 @@ if __name__ == "__main__":
 
     assert solution.intersect(nums1 = [1,2,2,1], nums2 = [2,2]) == [2, 2]
 
-    assert solution.intersect(nums1 = [4,9,5], nums2 = [9,4,9,8,4]) == [4, 9]
+    assert solution.intersect(nums1 = [4,9,5], nums2 = [9,4,9,8,4]) == [9, 4]
 
     assert solution.intersect(nums1 = [1], nums2 = [1]) == [1]
 
