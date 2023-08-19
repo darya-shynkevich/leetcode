7a890d9350d3d2b4e1bb8406937eb686deb7c760
Darya Shynkevich <shynkevich.darya@gmail.com>
2023-08-19 10:05:43 +0200

    Practice 2 pointers: 977

---
+++ algorithms/two_pointers/977.py
@@ -0,0 +1,14 @@
+import collections
+from typing import List
+
+
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        pass
+
+
+
+if __name__ == "__main__":
+    solution = Solution()
+
+    assert solution.sortedSquares(nums = [-4,-1,0,3,10]) == [0,1,9,16,100]
