e91e680a4cc6a08344823f904766653be0f006ff
Darya Shynkevich <shynkevich.darya@gmail.com>
2023-08-30 00:25:20 +0200

    Practice 2 pointers: fix 1385 test cases

--- algorithms/two_pointers/1385.py
+++ algorithms/two_pointers/1385.py
@@ -23,10 +23,10 @@ class Solution:
 if __name__ == "__main__":
     solution = Solution()
 
-    # assert solution.findTheDistanceValue(arr1 = [4,5,8], arr2 = [10,9,1,8], d = 2) == 2
-    #
-    # assert solution.findTheDistanceValue(arr1 = [1,4,2,3], arr2 = [-4,-3,6,10,20,30], d = 3) == 2
-    #
-    # assert solution.findTheDistanceValue(arr1 = [2,1,100,3], arr2 = [-5,-2,10,-3,7], d = 6) == 1
+    assert solution.findTheDistanceValue(arr1 = [4,5,8], arr2 = [10,9,1,8], d = 2) == 2
+
+    assert solution.findTheDistanceValue(arr1 = [1,4,2,3], arr2 = [-4,-3,6,10,20,30], d = 3) == 2
+
+    assert solution.findTheDistanceValue(arr1 = [2,1,100,3], arr2 = [-5,-2,10,-3,7], d = 6) == 1
 
     assert solution.findTheDistanceValue(arr1=[-3,-3,4,-1,-10], arr2=[7,10], d=12) == 1
